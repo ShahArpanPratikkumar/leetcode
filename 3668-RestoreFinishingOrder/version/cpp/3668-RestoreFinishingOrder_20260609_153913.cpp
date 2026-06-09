4f6312372ed0a1380f9684f8883afbb133a938fe
Arpan Shah <shaharpan.cg@gmail.com>
2026-06-09 15:39:16 +0530

    Create 3668-RestoreFinishingOrder

---
+++ 3668-RestoreFinishingOrder/version/cpp/3668-RestoreFinishingOrder_20260609_153913.cpp
@@ -0,0 +1,17 @@
+// Last updated: 09/06/2026, 15:39:13
+1class Solution {
+2public:
+3    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+4        vector<int> ans;
+5
+6        for (int x : order){
+7            for (int f : friends){
+8                if( x == f){
+9                    ans.push_back(x);
+10                }
+11            }
+12
+13        }
+14        return ans ;
+15    }
+16};
