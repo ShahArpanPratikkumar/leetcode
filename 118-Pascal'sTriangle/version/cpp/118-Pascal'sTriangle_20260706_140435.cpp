f06e79380e807ea4ea2790c16d1d04bf03da6921
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-06 14:04:37 +0530

    Create 118-Pascal'sTriangle

---
+++ 118-Pascal'sTriangle/version/cpp/118-Pascal'sTriangle_20260706_140435.cpp
@@ -0,0 +1,16 @@
+// Last updated: 06/07/2026, 14:04:35
+1class Solution {
+2public:
+3    vector<int> getRow(int rowIndex) {
+4        vector<int> row(rowIndex + 1, 0);
+5        row[0] = 1;
+6
+7        for (int i = 1; i <= rowIndex; i++) {
+8            for (int j = i; j >= 1; j--) {
+9                row[j] += row[j - 1];
+10            }
+11        }
+12
+13        return row;
+14    }
+15};
