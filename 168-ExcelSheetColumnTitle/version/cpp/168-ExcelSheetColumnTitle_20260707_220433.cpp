50f9bafa3e14d2087b0c876423c22dc30bbf2697
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-07 22:04:34 +0530

    Create 168-ExcelSheetColumnTitle

---
+++ 168-ExcelSheetColumnTitle/version/cpp/168-ExcelSheetColumnTitle_20260707_220433.cpp
@@ -0,0 +1,15 @@
+// Last updated: 07/07/2026, 22:04:33
+1class Solution {
+2public:
+3    string convertToTitle(int columnNumber) {
+4        string ans = "";
+5
+6        while (columnNumber > 0) {
+7            columnNumber--;   // Make it 0-based
+8            ans = char('A' + (columnNumber % 26)) + ans;
+9            columnNumber /= 26;
+10        }
+11
+12        return ans;
+13    }
+14};
