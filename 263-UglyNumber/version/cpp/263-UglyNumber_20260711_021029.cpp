3f0c989d27d30047faff1c0b44c797a0ad78b7e2
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-11 02:10:29 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/version/cpp/263-UglyNumber_20260711_021029.cpp
@@ -0,0 +1,19 @@
+// Last updated: 11/07/2026, 02:10:29
+1class Solution {
+2public:
+3    int missingNumber(vector<int>& nums) {
+4        int n = nums.size();
+5        
+6        // Calculate the expected sum of numbers from 0 to n
+7        int expectedSum = n * (n + 1) / 2;
+8        
+9        // Calculate the actual sum of elements in the array
+10        int actualSum = 0;
+11        for (int num : nums) {
+12            actualSum += num;
+13        }
+14        
+15        // The difference is the missing number
+16        return expectedSum - actualSum;
+17    }
+18};
