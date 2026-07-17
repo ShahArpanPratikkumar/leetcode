b51a70bb22f264cedd1ea5ca01dc41137e6840eb
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-17 12:53:46 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/version/cpp/704-BinarySearch_20260717_125345.cpp
@@ -0,0 +1,12 @@
+// Last updated: 17/07/2026, 12:53:45
+1class Solution {
+2public:
+3    int search(vector<int>& nums, int target) {
+4        for (int i = 0; i < nums.size(); i++) {
+5            if (nums[i] == target) {
+6                return i;
+7            }
+8        }    
+9            return -1;
+10    }   
+11};
