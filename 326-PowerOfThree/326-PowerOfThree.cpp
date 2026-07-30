0c8ae62d78717e094f0aa74edfc411c7663b5ff8
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-30 10:07:32 +0530

    Create 326-PowerOfThree

---
+++ 326-PowerOfThree/326-PowerOfThree.cpp
@@ -0,0 +1,11 @@
+// Last updated: 30/07/2026, 10:07:31
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
