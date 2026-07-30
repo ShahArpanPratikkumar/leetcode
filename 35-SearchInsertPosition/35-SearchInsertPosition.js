0a5ea575db7c396f5f3a4e9eb99751a150a080ac
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-30 10:08:50 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.js
@@ -0,0 +1,10 @@
+// Last updated: 30/07/2026, 10:08:49
+var searchInsert = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return left;
+};
