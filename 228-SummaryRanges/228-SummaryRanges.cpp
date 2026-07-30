8ca6b3aee239aa401e8b9115757a699d9e8b532a
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-30 10:07:51 +0530

    Create 228-SummaryRanges

---
+++ 228-SummaryRanges/228-SummaryRanges.cpp
@@ -0,0 +1,23 @@
+// Last updated: 30/07/2026, 10:07:50
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int j = i;
+
+            while (j + 1 < nums.size() && nums[j + 1] == nums[j] + 1)
+                j++;
+
+            if (i == j)
+                ans.push_back(to_string(nums[i]));
+            else
+                ans.push_back(to_string(nums[i]) + "->" + to_string(nums[j]));
+
+            i = j;
+        }
+
+        return ans;
+    }
+};
