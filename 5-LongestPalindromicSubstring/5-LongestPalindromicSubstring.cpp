07a34d9d322862aca05bf745f3221b9440bbab9d
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-30 10:09:22 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
@@ -0,0 +1,24 @@
+// Last updated: 30/07/2026, 10:09:21
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res = "";
+        for (int i = 0; i < s.size(); i++) {
+            // check odd length
+            string odd = expand(s, i, i);
+            if (odd.size() > res.size()) res = odd;
+            // check even length
+            string even = expand(s, i, i + 1);
+            if (even.size() > res.size()) res = even;
+        }
+        return res;
+    }
+
+private:
+    string expand(const string &s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--; right++;
+        }
+        return s.substr(left + 1, right - left - 1);
+    }
+};
