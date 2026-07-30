7b3460ed838b8bd2b0f39bdc6cb521aadc66b16e
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-30 10:08:33 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ 104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,9 @@
+// Last updated: 30/07/2026, 10:08:32
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
