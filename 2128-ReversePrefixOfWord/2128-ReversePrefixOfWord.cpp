38c1e592f860c1baefacf6de4cada9b7d95068a0
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-30 10:06:29 +0530

    Create 2128-ReversePrefixOfWord

---
+++ 2128-ReversePrefixOfWord/2128-ReversePrefixOfWord.cpp
@@ -0,0 +1,13 @@
+// Last updated: 30/07/2026, 10:06:28
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        for (int i = 0; i < word.size(); i++) {
+            if (word[i] == ch) {
+                reverse(word.begin(), word.begin() + i + 1);
+                break;
+            }
+        }
+        return word;
+    }
+};
