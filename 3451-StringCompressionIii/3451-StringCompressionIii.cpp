680a9c1ff2e7018254fb6db14dc436205e973ba1
Arpan Shah <shaharpan.cg@gmail.com>
2026-07-31 13:18:17 +0530

    Create 3451-StringCompressionIii

---
+++ 3451-StringCompressionIii/3451-StringCompressionIii.cpp
@@ -0,0 +1,23 @@
+// Last updated: 31/07/2026, 13:18:16
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+        int n = word.length();
+        int i = 0;
+
+        while (i < n) {
+            char c = word[i];
+            int count = 0;
+
+            while (i < n && word[i] == c && count < 9) {
+                count++;
+                i++;
+            }
+
+            comp += to_string(count) + c;
+        }
+
+        return comp;
+    }
+};
