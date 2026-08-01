e5ad4854d0e5fd6f0d1197adbacb89a3e2b74335
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-01 21:28:51 +0530

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/495-TeemoAttacking.cpp
@@ -0,0 +1,18 @@
+// Last updated: 01/08/2026, 21:28:52
+class Solution {
+public:
+    int findPoisonedDuration(std::vector<int>& timeSeries, int duration) {
+        if (timeSeries.empty()) return 0;
+
+        int totalPoisonedTime = 0;
+        int n = timeSeries.size();
+
+        for (int i = 0; i < n - 1; ++i) {
+            totalPoisonedTime += std::min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+
+        totalPoisonedTime += duration;
+
+        return totalPoisonedTime;
+    }
+};
