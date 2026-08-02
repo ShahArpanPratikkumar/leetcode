fd2106c6c58fe7cd3d8cf1a580010a6217431995
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-02 20:37:38 +0530

    Create 504-Base7

---
+++ 504-Base7/504-Base7.cpp
@@ -0,0 +1,18 @@
+// Last updated: 02/08/2026, 20:37:38
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if (num == 0) return "0";
+        
+        bool isNegative = num < 0;
+        num = abs(num);
+        string result = "";
+        
+        while (num > 0) {
+            result = to_string(num % 7) + result;
+            num /= 7;
+        }
+        
+        return isNegative ? "-" + result : result;
+    }
+};
