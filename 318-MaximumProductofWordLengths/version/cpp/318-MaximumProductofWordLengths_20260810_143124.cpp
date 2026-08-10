031b3932eb1ebe96c4e64e24f3954ccb0264ad72
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-10 14:31:26 +0530

    Create 318-MaximumProductofWordLengths

---
+++ 318-MaximumProductofWordLengths/version/cpp/318-MaximumProductofWordLengths_20260810_143124.cpp
@@ -0,0 +1,26 @@
+// Last updated: 10/08/2026, 14:31:24
+1class Solution {
+2public:
+3    int maxProduct(vector<string>& words) {
+4        int n = words.size();
+5        vector<int> masks(n, 0);
+6        
+7        for (int i = 0; i < n; ++i) {
+8            for (char c : words[i]) {
+9                masks[i] |= (1 << (c - 'a'));
+10            }
+11        }
+12        
+13        int max_prod = 0;
+14        for (int i = 0; i < n; ++i) {
+15            for (int j = i + 1; j < n; ++j) {
+16                if ((masks[i] & masks[j]) == 0) {
+17                    int prod = words[i].length() * words[j].length();
+18                    max_prod = max(max_prod, prod);
+19                }
+20            }
+21        }
+22        
+23        return max_prod;
+24    }
+25};
