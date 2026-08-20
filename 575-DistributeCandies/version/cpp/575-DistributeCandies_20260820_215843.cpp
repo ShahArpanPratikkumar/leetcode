370de15636444bea677a55702baea4636842f194
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-20 21:58:44 +0530

    Create 575-DistributeCandies

---
+++ 575-DistributeCandies/version/cpp/575-DistributeCandies_20260820_215843.cpp
@@ -0,0 +1,8 @@
+// Last updated: 20/08/2026, 21:58:43
+1class Solution {
+2public:
+3    int distributeCandies(vector<int>& candyType) {
+4        unordered_set<int> uniqueTypes(candyType.begin(), candyType.end());
+5        return min(uniqueTypes.size(), candyType.size() / 2);
+6    }
+7};
