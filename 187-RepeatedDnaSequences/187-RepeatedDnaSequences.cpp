9e7dc3934877d48ee680e95b622d78a02eaf9ab9
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-25 10:47:18 +0530

    Create 187-RepeatedDnaSequences

---
+++ 187-RepeatedDnaSequences/187-RepeatedDnaSequences.cpp
@@ -0,0 +1,23 @@
+// Last updated: 25/08/2026, 10:47:17
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_set<string> seen;
+        unordered_set<string> repeated;
+        
+        if (s.length() < 10) {
+            return {};
+        }
+        
+        for (int i = 0; i <= static_cast<int>(s.length()) - 10; ++i) {
+            string sub = s.substr(i, 10);
+            if (seen.count(sub)) {
+                repeated.insert(sub);
+            } else {
+                seen.insert(sub);
+            }
+        }
+        
+        return vector<string>(repeated.begin(), repeated.end());
+    }
+};
