5c4f5d612ba3842a07212c03f9493df98df6d263
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-25 10:45:55 +0530

    Create 566-ReshapeTheMatrix

---
+++ 566-ReshapeTheMatrix/566-ReshapeTheMatrix.cpp
@@ -0,0 +1,21 @@
+// Last updated: 25/08/2026, 10:45:55
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        // Check if reshape is possible
+        if (m * n != r * c) {
+            return mat;
+        }
+        
+        vector<vector<int>> ans(r, vector<int>(c));
+        
+        for (int i = 0; i < m * n; ++i) {
+            ans[i / c][i % c] = mat[i / n][i % n];
+        }
+        
+        return ans;
+    }
+};
