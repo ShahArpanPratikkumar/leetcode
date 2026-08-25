790e8d98fc9330323b4bd4ee32bd3ed599c5e690
Arpan Shah <shaharpan.cg@gmail.com>
2026-08-25 10:45:18 +0530

    Create 1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints

---
+++ 1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints/1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints.cpp
@@ -0,0 +1,17 @@
+// Last updated: 25/08/2026, 10:45:17
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+
+        int maxWidth = 0;
+        
+        for (size_t i = 1; i < points.size(); ++i) {
+            maxWidth = max(maxWidth, points[i][0] - points[i - 1][0]);
+        }
+
+        return maxWidth;
+    }
+};
